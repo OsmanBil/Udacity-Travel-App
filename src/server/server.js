0aba2fbe2e31b4f427fc96d1ebe803768fdf2d5a
OsmanBil <101802819+OsmanBil@users.noreply.github.com>
2023-06-27 11:16:50 +0200

    update

---
+++ src/server/server.js
@@ -0,0 +1,30 @@
+// Dotenv is a package for Node.js applications that makes it easy to use environment variables
+const dotenv = require('dotenv');
+dotenv.config();
+
+var path = require('path')
+const express = require('express')
+var bodyParser = require('body-parser')
+var cors = require('cors')
+
+
+const app = express()
+app.use(cors())
+// to use json
+app.use(bodyParser.json())
+// to use url encoded values
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+app.use(express.static('dist'))
+
+
+
+app.get('/', function (req, res) {
+    res.sendFile('dist/index.html')
+})
+
+// designates what port the app will listen to for incoming requests
+app.listen(8081, function () {
+    console.log('App listening on port 8081!')
+})
