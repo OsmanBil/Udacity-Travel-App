559f55c966b5563c3b4490c5b10cdcd9bc9902f5
OsmanBil <101802819+OsmanBil@users.noreply.github.com>
2023-06-27 12:35:25 +0200

    css added

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,13 @@
 // import { checkForName } from './js/nameChecker'
 // import { handleSubmit } from './js/formHandler'
 
-
+import './styles/body.scss'
+import './styles/header.scss'
 import './styles/main.scss'
+import './styles/footer.scss'
+import './styles/trip.scss'
+
+
 
 // console.log(checkForName);
 
